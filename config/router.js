ac7eb6fd3bf584b8f447e1584928ba71119060be
alyssachelseah <mnblaquera@up.edu.ph>
2015-08-26 12:24:37 +0800

    week3

--- config/router.js
+++ config/router.js
@@ -4,11 +4,17 @@ var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){
    router.route('/students')
-   .get(student.find);
+   .get(student.find)
+   .post(student.insert);
+   
+   router.route('/students/:id')
+   .get(student.findOne)
+   .put(student.update)
+   .delete(student.remove);
    return router;
-
 };
 
+/*
 module.exports = function(router){
    router.route('/teacher')
    .get(teacher.find)
@@ -16,3 +22,4 @@ module.exports = function(router){
    
    return router;
 };
+*/
